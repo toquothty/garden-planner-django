ab49266a73aec3ec4287177bbda0a07ca3e6675c
Thomas Toquothty <tltoquothty@gmail.com>
2023-08-23 13:43:42 -0400

    Create initial DB model

--- config/garden/models.py
+++ config/garden/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Garden_DB(models.Model):
+    __tablename__ = "garden_DB"
+
+    # id = Column(Integer, primary_key=True)
+    vegetable = models.CharField(max_length=200)
+    sow_type = models.CharField(max_length=20)  # Direct, Indoors, key used to filter later
+    harvest_days = models.IntegerField()  # How many days to harvest
+    plant_spacing = models.IntegerField()  # Inches between plants
+    seed_depth = models.IntegerField()  # Inches for seed depth
+    sow_window_start = models.DateField()  # Window to sow, regardless of sow type
+    sow_window_end = models.DateField()  # Window to sow, regardless of sow type
+    transplant_window_start = models.DateField()  # Window to transplant if applicable
+    transplant_window_end = models.DateField()  # Window to transplant if applicable
+    harvest_window_start = models.DateField()  # Window to harvest
+    harvest_window_end = models.DateField()  # Window to harvest
+    vegetable_picture_url = models.CharField(max_length=500)  # Public URL for vegetable picture
+    date_created = models.DateTimeField(auto_now_add=True)
