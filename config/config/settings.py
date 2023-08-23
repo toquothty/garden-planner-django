7c5c5c13e43f80d2176ccd662d30daea85928303
Thomas Toquothty <tltoquothty@gmail.com>
2023-08-23 13:43:21 -0400

    Add in django extensions in order to do some scripts

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'garden'
+    'garden.apps.GardenConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
